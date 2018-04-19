eef5a4ba584d112021b24a11f90191b920ff8aee
Tom Bealby <tbealby@centiq.co.uk>
2018-04-19 07:30:59 +0100

    Shopping cart (6) : implement simple offers in cost calculator.

--- src/model/CostCalculatorSimpleOffersImpl.java
+++ src/model/CostCalculatorSimpleOffersImpl.java
@@ -1,13 +1,33 @@
 package model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CostCalculatorSimpleOffersImpl implements CostCalculator {
 
 	@Override
 	public int calculateCost(List<String> basket) {
-		// TODO Auto-generated method stub
-		return 0;
+		int totalCost = 0;
+		Map<String, Integer> nameQuantityMap = new HashMap<>();
+		for (String item: basket) {
+			if (nameQuantityMap.containsKey(item)) {
+				Integer num = nameQuantityMap.get(item);
+				nameQuantityMap.put(item, ++num);
+			} else {
+				nameQuantityMap.put(item, 1);
+			}
+		}
+
+		Map<String, PriceListDetail> priceList = PriceList.getPriceListWithOffers();
+		for (Map.Entry<String, Integer> entry : nameQuantityMap.entrySet()) {
+			PriceListDetail priceListDetail = priceList.get(entry.getKey());
+			DiscountCalculator discountCalculator = priceListDetail.getDiscountCalculator();
+			int itemPrice = priceListDetail.getPrice();
+			int intemQuantity = entry.getValue();
+			totalCost += discountCalculator.calculateCost(itemPrice, intemQuantity);
+		}
+		return totalCost;
 	}
 
 }
--- src/model/PriceList.java
+++ src/model/PriceList.java
@@ -16,8 +16,8 @@ public class PriceList {
 	public static Map<String, PriceListDetail> getPriceListWithOffers() {
 		//should priceList be a singleton?
 		Map<String, PriceListDetail> priceList = new HashMap<String, PriceListDetail>();
-//		priceList.put("Apple", 60);
-//		priceList.put("Orange", 25);
+		priceList.put("Apple", new PriceListDetail(60, new DiscountCalculatorBuyOneGetOneFreeImpl()));
+		priceList.put("Orange", new PriceListDetail(25, new DiscountCalculatorThreeForPriceOfTwoImpl()));
 		return priceList;
 	}
 
