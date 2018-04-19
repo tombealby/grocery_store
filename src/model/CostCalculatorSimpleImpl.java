341e45f970e0be607c2db50f481ab487fb7fe628
Tom Bealby <tbealby@centiq.co.uk>
2018-04-19 06:32:48 +0100

    Shopping cart (2) : Add implementation of calculateCost.

--- src/model/CostCalculatorSimpleImpl.java
+++ src/model/CostCalculatorSimpleImpl.java
@@ -1,13 +1,18 @@
 package model;
 
 import java.util.List;
+import java.util.Map;
 
 public class CostCalculatorSimpleImpl implements CostCalculator {
 
 	@Override
 	public int calculateCost(List<String> basket) {
-		// TODO Auto-generated method stub
-		return 0;
+		int totalCost = 0;
+		Map<String, Integer> priceList = PriceList.getPriceList();
+		for (String item: basket) {
+			totalCost += priceList.get(item);
+		}
+		return totalCost;
 	}
 
 }
